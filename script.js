c528049811d5a34d31c2bd4f11ad8259f57ee8b9
Arthur de Carvalho Miranda <104659615+arthurmiranda01@users.noreply.github.com>
2023-05-11 19:43:25 -0300

    Roll Dice
    adicionando a mecânica de rolar o dado

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ stage.start(
     document.querySelector('#character'),
     document.querySelector('#monster')
 )
+
+function rollDice(){
+
+    var imagens = [
+        "https://dado.online/dado1.png", 
+        "https://dado.online/dado2.png",
+        "https://dado.online/dado3.png",
+        "https://dado.online/dado4.png",
+        "https://dado.online/dado5.png",
+        "https://dado.online/dado6.png"
+    ];
+  
+          var imagemAleatoria = imagens[Math.floor(Math.random() * imagens.length)];
+          var img = document.getElementById("imagem");
+          img.src = imagemAleatoria;
+  
+  
+  }
